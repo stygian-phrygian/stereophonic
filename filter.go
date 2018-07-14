41339e98c865df8ef78b206bea82bb621874f040
stygian-phrygian <stygian-phrygian@users.noreply.github.com>
2018-07-13 22:20:44 -0400

    (re)updated the filter code to be 24db per octave (slighly modified), also corrected future bugs in the methods

--- filter.go
+++ filter.go
@@ -4,10 +4,6 @@ import (
 	"math"
 )
 
-var (
-	maxCutoff float64 = math.Nextafter(1.0, 0.0)
-)
-
 // a "simple" 12db per octave resonant filter algorithm
 //
 //
@@ -34,8 +30,8 @@ type filter struct {
 	// nb, cutoff cannot (ever) equal 1
 	cutoff, resonance float64
 	// these values are used in the actual IIR filter computation
-	feedback   float64
-	buf0, buf1 float64
+	feedback               float64
+	buf0, buf1, buf2, buf3 float64
 }
 
 func newFilter() (*filter, error) {
@@ -53,20 +49,25 @@ func newFilter() (*filter, error) {
 }
 
 func (f *filter) calculateFeedback() {
-	f.feedback = f.resonance + f.resonance/(1.0-f.cutoff)
+	f.feedback = math.Min(f.resonance+f.resonance/(1.0-f.cutoff), 1.0)
 }
 
 // filter the input
 func (f *filter) tick(input float64) float64 {
-	f.buf0 += f.cutoff * (input - f.buf0 + f.feedback*(f.buf0-f.buf1))
+	// ordinarily ----------------------------------> (this term    )
+	// is buf0 - buf3, but I chose buf0 - buf2... seemed to ease back
+	// the filter instability a bit
+	f.buf0 += f.cutoff * (input - f.buf0 + f.feedback*(f.buf0-f.buf2))
 	f.buf1 += f.cutoff * (f.buf0 - f.buf1)
+	f.buf2 += f.cutoff * (f.buf1 - f.buf2)
+	f.buf3 += f.cutoff * (f.buf2 - f.buf3)
 	switch f.filterMode {
 	case LPFilter:
-		return f.buf1
+		return f.buf3
 	case HPFilter:
-		return input - f.buf1
+		return input - f.buf3
 	case BPFilter:
-		return f.buf0 - f.buf1
+		return f.buf0 - f.buf3
 	default: // NoFilter
 		return input
 	}
@@ -87,8 +88,6 @@ func (f *filter) setCutoff(cutoff float64) {
 
 func (f *filter) setResonance(resonance float64) {
 	// make sure resonance is always 0 to 1
-	if 0 <= resonance && resonance <= 1 {
-		f.resonance = resonance
-		f.calculateFeedback()
-	}
+	f.resonance = math.Max(math.Min(resonance, 0.9999999999999999), 0.0)
+	f.calculateFeedback()
 }
